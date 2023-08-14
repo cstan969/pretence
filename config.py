7de7da5f8681ba3e35243e5d4f11eadba4505330
Carl <cstan969@gmail.com>
2023-08-14 12:14:01 -0400

    minor config changes

--- config.py
+++ config.py
@@ -23,15 +23,8 @@ GPT4 = ChatOpenAI(model='gpt-4')
 TURBO = ChatOpenAI(model='gpt-3.5-turbo')
 VICUNA = 'vicuna-7b-1.1-16bit'
 STARCODER = 'starcoder'
-LLAMA2_RP = LlamaCpp(
-            model_path="/home/carl/Pretence/models/llama2-chronos-hermes-13b/chronos-hermes-13b-v2.ggmlv3.q5_0.bin",
-            n_gpu_layers=50,
-            n_batch=512,
-            input={"temperature": 0.75, "max_length": 2000, "top_p": 1},
-            callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-            verbose=True,
-            n_ctx=2000,
-        )
+LLAMA2_RP = "llama2"
+
 
 
 
@@ -41,6 +34,17 @@ EXTRACTOR_MODEL= TURBO
 GameDesigner_model = TURBO
 NpcUserInteraction_model = TURBO
 NPCBUILDER_MODEL = LlamaCpp
+
+if NpcUserInteraction_model == "llama2":
+    NpcUserInteraction_model = LlamaCpp(
+            model_path="/home/carl/Pretence/models/llama2-chronos-hermes-13b/chronos-hermes-13b-v2.ggmlv3.q5_0.bin",
+            n_gpu_layers=50,
+            n_batch=512,
+            input={"temperature": 0.75, "max_length": 2000, "top_p": 1},
+            callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+            verbose=True,
+            n_ctx=2000,
+        )
 
 server = "localhost"
 IS_SERVER_OR_CLIENT = server
